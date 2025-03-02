f6efce027e2b5ddaf79328317dbb5372a57cf230
anhhao26 <anhhaoqk@gmail.com>
2025-03-02 20:07:27 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import HomeView from './Views/HomeView.vue'
+import PokemonDetail from './components/PokemonDetail/PokemonDetail.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { 
+    path: '/:pokemonName', 
+    name: 'PokemonDetail', 
+    component: PokemonDetail, 
+    props: true 
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
